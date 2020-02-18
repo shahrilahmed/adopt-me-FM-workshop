02a58a38e46848a105f96474612bc04b3ce6d3d3
Shahril Ahmed Siddiquee <shahrilahmed@gmail.com>
2020-02-19 00:52:23 +0600

    commit on 'master'

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -15,6 +20,8 @@ const SearchParams = () => {
             onChange={event => setLocation(event.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
